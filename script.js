f72e18322a051d92a8b8112a7801c58052ff47dc
Gafuroff-prog <gafuroffgaming@gmail.com>
2024-11-18 13:55:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let person = {}
+person.name = prompt('Ismingizni kiriting..');
+person.id = prompt('Mars space id yingizni kiriting..');
+person.password = prompt('Mars space parolingizni kiriting..');
+console.log(person);
+
